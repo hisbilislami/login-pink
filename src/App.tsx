99b3fe68c5618a3fea0ec6e626a3fab0d04891b5
Hisbil Islami <hisbil.prog@gmail.com>
2026-01-24 16:53:30 +0700

    add color & font

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-red-500 flex justify-center items-center">
-        <p>Hello world</p>
+      <div className="min-h-screen bg-pink-bubblegum flex flex-col justify-center items-center">
+        <p className="">Hello world</p>
+        <Button size="sm" className="bg-wisteria-blue">
+          Oke
+        </Button>
+        <h1>Hisbil Islami</h1>
       </div>
     </>
   );
